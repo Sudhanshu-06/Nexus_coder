8d3cb4ea5d42850773c07753e45291ae38993baa
Sudhanshu-06 <sudhanshu767778@gmail.com>
2026-01-15 14:04:01 +0530

    class code done

---
+++ 3. JavaScript/Day_04/script.js
@@ -0,0 +1,39 @@
+// comparsion operator
+let a1=1;
+let a2 =2;
+console.log(a1==a2)
+
+console.log(10 > 5); // true
+console.log(10 < 5); // false
+
+console.log(10 >= 5); // true
+console.log(10 <= 5); // false
+
+console.log(10==10); // true
+
+console.log("/////////")
+
+let num= 10;
+let str= "20";
+console.log(num==str);
+// Type conversion hoga string to number
+
+//  null==undefined  //true
+//  null===undefined  //false
+
+console.log("//////")
+
+console.log(null==0);
+console.log(null<0);
+console.log(null>0);
+console.log(null<=0);
+console.log(null>=0);
+
+
+console.log("/////////")
+// Logical operator
+
+let age = 18;
+let money = 420;
+console.log(age>=18 && money>200);
+console.log(age>=18 || money>500);
